c0b21bf7db788cdf2864d5b7fef5eba12daaf172
Quinn <quinnvoker@gmail.com>
2020-07-30 21:39:20 +0000

    ip fetcher functional

---
+++ iss.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org/?format=json', (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length < 1) {
+      callback('Did not recieve valid IP object');
+      return;
+    }
+    callback(error, data.ip);
+  });
+};
+
+module.exports = { fetchMyIP };
